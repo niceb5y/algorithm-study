339276fa7825dad820d6dbc961a777a86866f211
Jongkeun Choi <ynifamily3@gmail.com>
2019-01-07 15:52:35 +0900

    Try #1

--- bj-room-number-1475/solve.c
+++ bj-room-number-1475/solve.c
@@ -5,6 +5,33 @@
 
 int main(int argc, char *argv[])
 {
-    printf("%d\n", util_abs(-6));
+    int i, counter_max = 0, counter_another_max = 0;
+    char N[8]; // 방 번호 N (0 ~ 1,000,000)
+    int counter[10] = {0, }; // 0 ~ 9 가 몇 개 필요한지를 센다
+    scanf("%s", N);
+    for (i = 0; N[i] != '\0'; i++) { // 999966 -> 3
+        if(N[i] - '0' == 6 || N[i] - '0' == 9) {
+            counter[N[i] - '0'] += 1;
+            counter_max = counter_max < counter[6]+counter[9] ? counter[6]+counter[9] : counter_max;
+        }
+        else {
+            counter[N[i] - '0'] += 1;
+            counter_another_max = counter_another_max < counter[N[i] - '0'] ? counter[N[i] - '0'] : counter_another_max;
+        }
+        // printf("최대 횟수(69) : %d 최대 횟수(다른거) : %d\n", counter_max, counter_another_max);
+    }
+    /*
+    for (i = 0; i < 10; i++) {
+        printf("%d ", counter[i]);
+    }
+    */
+    putchar('\n');
+    if(counter_max * 2 <= counter_another_max) {
+        printf("%d\n", counter_another_max);
+    }
+    else {
+        printf("%d\n", counter_max / 2 + (counter_max % 2));
+    }
+
     return unlink(*argv);
 }

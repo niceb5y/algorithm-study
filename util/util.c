63e3e032991ff0352e7116112d82e62bf059a3cd
Jongkeun Choi <ynifamily3@gmail.com>
2019-01-07 14:27:53 +0900

    초기 환경 구성

---
+++ util/util.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include "util.h"
+
+int util_abs(const int n)
+{
+    return n > 0 ? n : -n;
+}
---
+++ util/util.h
@@ -0,0 +1,7 @@
+#ifndef ___UTIL
+#define ___UTIL
+
+/* returns absolute integer */
+int util_abs(const int n);
+
+#endif

85482fc570ecf0df791d52593c8e86abda066a54
Seungho Kim <niceb5y@gmail.com>
2019-01-13 17:06:49 +0900

    Refactor “util/new.js”

--- util/new.js
+++ util/new.js
@@ -4,104 +4,182 @@
  * Boilerplate generator
  * Copyright (c) 2019 Seungho Kim.
  */
-var readline = require("readline");
-var fs = require("fs");
-var path = require("path");
-var cheerio = require("cheerio");
-var axios = require("axios");
-var figlet = require("figlet");
-var chalk = require("chalk");
-var ora = require("ora");
-var rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
-console.clear();
-console.log(chalk.blue(figlet.textSync("Util/New")));
-console.log();
-rl.question("어떤 문제를 푸시겠습니까?: ", function (problem) {
-    var problemNum = problem.replace("https://www.acmicpc.net/problem/", "");
-    rl.close();
-    if (problemNum < 1000 ||
-        isNaN(parseInt(problemNum)) ||
-        !isFinite(problemNum)) {
-        console.error("올바르지 않은 입력입니다.");
-        process.exit(1);
-    }
-    console.log();
-    console.log(chalk.yellow("작업을 시작합니다."));
-    console.log();
-    var projectPath = path.join(__dirname, "..");
-    var newDirNum = 1;
-    while (fs.existsSync(path.join(projectPath, "" + newDirNum))) {
-        ++newDirNum;
-    }
-    var step1 = ora("\uB514\uB809\uD1A0\uB9AC " + newDirNum + " \uC0DD\uC131").start();
-    fs.mkdirSync(path.join(projectPath, "" + newDirNum));
-    step1.succeed();
-    var step2 = ora("\uB514\uB809\uD1A0\uB9AC " + newDirNum + "/test \uC0DD\uC131").start();
-    fs.mkdirSync(path.join(projectPath, newDirNum + "/test"));
-    step2.succeed();
-    var step3 = ora("\uD30C\uC77C " + newDirNum + "/Makefile \uC0DD\uC131").start();
-    fs.copyFileSync(path.join(__dirname, "/templates/Makefile"), path.join(projectPath, "" + newDirNum, "Makefile"));
-    step3.succeed();
-    var step4 = ora("\uD30C\uC77C " + newDirNum + "/main.cpp \uC0DD\uC131").start();
-    fs.copyFileSync(path.join(__dirname, "/templates/main.cpp"), path.join(projectPath, "" + newDirNum, "main.cpp"));
-    step4.succeed();
-    axios
-        .get("https://www.acmicpc.net/problem/" + problemNum)
-        .then(function (res) {
-        var body = res.data;
-        var $ = cheerio.load(body);
-        var htmlTitle = $("title").text();
-        var step5 = ora(htmlTitle + " \uBB38\uC81C \uC815\uBCF4 \uB2E4\uC6B4\uB85C\uB4DC").start();
-        step5.succeed();
-        var problemTitle = htmlTitle.split(": ")[1];
-        var sampleInput = 1, sampleOutput = 1;
-        while ($("#sample-input-" + sampleInput).length > 0) {
-            var text = $("#sample-input-" + sampleInput).text();
-            if (text === "") {
-                ++sampleInput;
-                continue;
-            }
-            if (!text.endsWith("\n"))
-                text += "\n";
-            fs.writeFileSync(path.join(projectPath, "" + newDirNum, "test/" + sampleInput + ".input"), text);
-            var step6 = ora("\uD30C\uC77C " + newDirNum + "/test/" + sampleInput + " \uC0DD\uC131").start();
-            step6.succeed();
-            ++sampleInput;
-        }
-        while ($("#sample-output-" + sampleOutput).length > 0) {
-            var text = $("#sample-output-" + sampleOutput).text();
-            if (!text.endsWith("\n"))
-                text += "\n";
-            fs.writeFileSync(path.join(projectPath, "" + newDirNum, "test/" + sampleOutput + ".answer"), text);
-            var step7 = ora("\uD30C\uC77C " + newDirNum + "/test/" + sampleOutput + " \uC0DD\uC131").start();
-            step7.succeed();
-            ++sampleOutput;
-        }
-        var step8 = ora("README.md \uC218\uC815").start();
-        var timezoneOffset = new Date().getTimezoneOffset() * 60000;
-        var today = new Date(Date.now() - timezoneOffset)
-            .toISOString()
-            .substring(0, 10);
-        var readme = fs
-            .readFileSync(path.join(projectPath, "README.md"))
-            .toString();
-        readme = readme.replace("\n\n## 참고 링크", "\n- " + today + " " + problemTitle + " [\\[\uBB38\uC81C\\]](https://www.acmicpc.net/problem/" + problemNum + ") [\\[\uCF54\uB4DC\\]](https://github.com/niceb5y/algorithm-study/blob/niceb5y/" + newDirNum + "/main.cpp)\n\n## \uCC38\uACE0 \uB9C1\uD06C");
-        fs.writeFileSync(path.join(projectPath, "README.md"), readme);
-        step8.succeed();
-        var step9 = ora(".travis.yml \uC218\uC815").start();
-        var travisConf = fs
-            .readFileSync(path.join(projectPath, ".travis.yml"))
-            .toString();
-        travisConf = travisConf.replace("\"cd " + (newDirNum - 1) + "\"", "\"cd " + newDirNum + "\"");
-        fs.writeFileSync(path.join(projectPath, ".travis.yml"), travisConf);
-        step9.succeed();
-        console.log();
-        console.log(chalk.green("\u2728 \uC791\uC5C5 \uC644\uB8CC \u2728"));
-        console.log();
-        console.log(chalk.blue("'cd " + newDirNum + "'(\uC73C)\uB85C " + problemTitle + "\uC758 \uD480\uC774\uB97C \uC2DC\uC791\uD558\uC138\uC694."));
-        console.log();
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
-});
+};
+var __generator = (this && this.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
+    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (_) try {
+            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [op[0] & 2, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+    }
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var axios_1 = __importDefault(require("axios"));
+var chalk_1 = __importDefault(require("chalk"));
+var cheerio_1 = __importDefault(require("cheerio"));
+var figlet_1 = __importDefault(require("figlet"));
+var fs_1 = __importDefault(require("fs"));
+var ora_1 = __importDefault(require("ora"));
+var path_1 = __importDefault(require("path"));
+var readline_1 = __importDefault(require("readline"));
+var util_1 = __importDefault(require("util"));
+function ask(query) {
+    return __awaiter(this, void 0, void 0, function () {
+        var rl, result;
+        return __generator(this, function (_a) {
+            switch (_a.label) {
+                case 0:
+                    rl = readline_1.default.createInterface({
+                        input: process.stdin,
+                        output: process.stdout
+                    });
+                    return [4 /*yield*/, util_1.default.promisify(function (question, callback) {
+                            rl.question(question, callback.bind(null, null));
+                        })(query)];
+                case 1:
+                    result = _a.sent();
+                    rl.close();
+                    return [2 /*return*/, result];
+            }
+        });
+    });
+}
+function init() {
+    return __awaiter(this, void 0, void 0, function () {
+        return __generator(this, function (_a) {
+            console.clear();
+            console.log(chalk_1.default.blue(figlet_1.default.textSync("Util/New")));
+            console.log();
+            return [2 /*return*/];
+        });
+    });
+}
+function main() {
+    return __awaiter(this, void 0, void 0, function () {
+        var problemNum, _a, _b, projectPath, newDirNum, step1, step2, step3, step4, step5, res, body, $, htmlTitle, step6, sampleInput, sampleOutput, text, step7, text, step8, timezoneOffset, today, readme, problemTitle, step9, travisConf;
+        return __generator(this, function (_c) {
+            switch (_c.label) {
+                case 0:
+                    init();
+                    _a = parseInt;
+                    if (!(process.argv.length == 3)) return [3 /*break*/, 1];
+                    _b = process.argv[2];
+                    return [3 /*break*/, 3];
+                case 1: return [4 /*yield*/, ask("어떤 문제를 푸시겠습니까?: ")];
+                case 2:
+                    _b = _c.sent();
+                    _c.label = 3;
+                case 3:
+                    problemNum = _a.apply(void 0, [(_b).replace("https://www.acmicpc.net/problem/", "")]);
+                    if (problemNum < 1000 || isNaN(problemNum) || !isFinite(problemNum)) {
+                        ora_1.default("올바르지 않은 입력입니다.").fail();
+                        process.exit(1);
+                    }
+                    console.log();
+                    console.log(chalk_1.default.yellow("작업을 시작합니다."));
+                    console.log();
+                    projectPath = path_1.default.join(__dirname, "..");
+                    newDirNum = 1;
+                    while (fs_1.default.existsSync(path_1.default.join(projectPath, "" + newDirNum))) {
+                        ++newDirNum;
+                    }
+                    step1 = ora_1.default("\uD504\uB85C\uC81D\uD2B8 \uB514\uB809\uD1A0\uB9AC \uC0DD\uC131").start();
+                    fs_1.default.mkdirSync(path_1.default.join(projectPath, "" + newDirNum));
+                    step1.succeed();
+                    step2 = ora_1.default("\uD504\uB85C\uC81D\uD2B8 \uD14C\uC2A4\uD2B8 \uB514\uB809\uD1A0\uB9AC \uC0DD\uC131").start();
+                    fs_1.default.mkdirSync(path_1.default.join(projectPath, newDirNum + "/test"));
+                    step2.succeed();
+                    step3 = ora_1.default("Makefile \uD30C\uC77C \uC0DD\uC131").start();
+                    fs_1.default.copyFileSync(path_1.default.join(__dirname, "/templates/Makefile"), path_1.default.join(projectPath, "" + newDirNum, "Makefile"));
+                    step3.succeed();
+                    step4 = ora_1.default("main.cpp \uD30C\uC77C \uC0DD\uC131").start();
+                    fs_1.default.copyFileSync(path_1.default.join(__dirname, "/templates/main.cpp"), path_1.default.join(projectPath, "" + newDirNum, "main.cpp"));
+                    step4.succeed();
+                    step5 = ora_1.default("\uBB38\uC81C \uC815\uBCF4 \uB2E4\uC6B4\uB85C\uB4DC").start();
+                    return [4 /*yield*/, axios_1.default.get("https://www.acmicpc.net/problem/" + problemNum)];
+                case 4:
+                    res = _c.sent();
+                    body = res.data;
+                    $ = cheerio_1.default.load(body);
+                    htmlTitle = $("title").text();
+                    step5.succeed(htmlTitle + " \uBB38\uC81C \uC815\uBCF4 \uB2E4\uC6B4\uB85C\uB4DC");
+                    step6 = ora_1.default("\uD14C\uC2A4\uD2B8 \uCF00\uC774\uC2A4 \uC785\uB825 \uC0DD\uC131").start();
+                    sampleInput = 1, sampleOutput = 1;
+                    while ($("#sample-input-" + sampleInput).length > 0) {
+                        text = $("#sample-input-" + sampleInput).text();
+                        if (text === "") {
+                            ++sampleInput;
+                            continue;
+                        }
+                        if (!text.endsWith("\n"))
+                            text += "\n";
+                        fs_1.default.writeFileSync(path_1.default.join(projectPath, "" + newDirNum, "test/" + sampleInput + ".input"), text);
+                        ++sampleInput;
+                    }
+                    step6.succeed();
+                    step7 = ora_1.default("\uD14C\uC2A4\uD2B8 \uCF00\uC774\uC2A4 \uCD9C\uB825 \uC0DD\uC131").start();
+                    while ($("#sample-output-" + sampleOutput).length > 0) {
+                        text = $("#sample-output-" + sampleOutput).text();
+                        if (!text.endsWith("\n"))
+                            text += "\n";
+                        fs_1.default.writeFileSync(path_1.default.join(projectPath, "" + newDirNum, "test/" + sampleOutput + ".answer"), text);
+                        ++sampleOutput;
+                    }
+                    step7.succeed();
+                    step8 = ora_1.default("README.md \uC218\uC815").start();
+                    timezoneOffset = new Date().getTimezoneOffset() * 60000;
+                    today = new Date(Date.now() - timezoneOffset)
+                        .toISOString()
+                        .substring(0, 10);
+                    readme = fs_1.default.readFileSync(path_1.default.join(projectPath, "README.md")).toString();
+                    problemTitle = htmlTitle.split(": ")[1];
+                    readme = readme.replace("\n\n## 참고 링크", "\n- " + today + " " + problemTitle + " [\\[\uBB38\uC81C\\]](https://www.acmicpc.net/problem/" + problemNum + ") [\\[\uCF54\uB4DC\\]](https://github.com/niceb5y/algorithm-study/blob/niceb5y/" + newDirNum + "/main.cpp)\n\n## \uCC38\uACE0 \uB9C1\uD06C");
+                    fs_1.default.writeFileSync(path_1.default.join(projectPath, "README.md"), readme);
+                    step8.succeed();
+                    step9 = ora_1.default(".travis.yml \uC218\uC815").start();
+                    travisConf = fs_1.default
+                        .readFileSync(path_1.default.join(projectPath, ".travis.yml"))
+                        .toString();
+                    travisConf = travisConf.replace("\"cd " + (newDirNum - 1) + "\"", "\"cd " + newDirNum + "\"");
+                    fs_1.default.writeFileSync(path_1.default.join(projectPath, ".travis.yml"), travisConf);
+                    step9.succeed();
+                    console.log();
+                    console.log(chalk_1.default.green("\u2728 \uC791\uC5C5 \uC644\uB8CC \u2728"));
+                    console.log();
+                    console.log(chalk_1.default.blue("'cd " + newDirNum + "'(\uC73C)\uB85C " + problemTitle + "\uC758 \uD480\uC774\uB97C \uC2DC\uC791\uD558\uC138\uC694."));
+                    console.log();
+                    return [2 /*return*/];
+            }
+        });
+    });
+}
+main();
--- util/src/new.ts
+++ util/src/new.ts
@@ -4,34 +4,51 @@
  * Copyright (c) 2019 Seungho Kim.
  */
 
-const readline = require("readline");
-const fs = require("fs");
-const path = require("path");
-const cheerio = require("cheerio");
-const axios = require("axios");
-const figlet = require("figlet");
-const chalk = require("chalk");
-const ora = require("ora");
+import axios from "axios";
+import chalk from "chalk";
+import cheerio from "cheerio";
+import figlet from "figlet";
+import fs from "fs";
+import ora from "ora";
+import path from "path";
+import readline from "readline";
+import util from "util";
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
-
-console.clear();
-console.log(chalk.blue(figlet.textSync("Util/New")));
-console.log();
-rl.question("어떤 문제를 푸시겠습니까?: ", (problem: any) => {
-  const problemNum = problem.replace("https://www.acmicpc.net/problem/", "");
+async function ask(query: string): Promise<string> {
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
+  const result = await util.promisify<string, string>(
+    (question: string, callback: Function) => {
+      rl.question(question, callback.bind(null, null));
+    }
+  )(query);
   rl.close();
-  if (
-    problemNum < 1000 ||
-    isNaN(parseInt(problemNum)) ||
-    !isFinite(problemNum)
-  ) {
-    console.error("올바르지 않은 입력입니다.");
+  return result;
+}
+
+async function init() {
+  console.clear();
+  console.log(chalk.blue(figlet.textSync("Util/New")));
+  console.log();
+}
+
+async function main() {
+  init();
+
+  const problemNum = parseInt(
+    (process.argv.length == 3
+      ? process.argv[2]
+      : await ask("어떤 문제를 푸시겠습니까?: ")
+    ).replace("https://www.acmicpc.net/problem/", "")
+  );
+
+  if (problemNum < 1000 || isNaN(problemNum) || !isFinite(problemNum)) {
+    ora("올바르지 않은 입력입니다.").fail();
     process.exit(1);
   }
+
   console.log();
   console.log(chalk.yellow("작업을 시작합니다."));
   console.log();
@@ -41,95 +58,94 @@ rl.question("어떤 문제를 푸시겠습니까?: ", (problem: any) => {
   while (fs.existsSync(path.join(projectPath, `${newDirNum}`))) {
     ++newDirNum;
   }
-  const step1 = ora(`디렉토리 ${newDirNum} 생성`).start();
+
+  const step1 = ora(`프로젝트 디렉토리 생성`).start();
   fs.mkdirSync(path.join(projectPath, `${newDirNum}`));
   step1.succeed();
-  const step2 = ora(`디렉토리 ${newDirNum}/test 생성`).start();
+
+  const step2 = ora(`프로젝트 테스트 디렉토리 생성`).start();
   fs.mkdirSync(path.join(projectPath, `${newDirNum}/test`));
   step2.succeed();
-  const step3 = ora(`파일 ${newDirNum}/Makefile 생성`).start();
+
+  const step3 = ora(`Makefile 파일 생성`).start();
   fs.copyFileSync(
     path.join(__dirname, "/templates/Makefile"),
     path.join(projectPath, `${newDirNum}`, "Makefile")
   );
   step3.succeed();
-  const step4 = ora(`파일 ${newDirNum}/main.cpp 생성`).start();
+
+  const step4 = ora(`main.cpp 파일 생성`).start();
   fs.copyFileSync(
     path.join(__dirname, "/templates/main.cpp"),
     path.join(projectPath, `${newDirNum}`, "main.cpp")
   );
   step4.succeed();
-  axios
-    .get(`https://www.acmicpc.net/problem/${problemNum}`)
-    .then((res: any) => {
-      const body = res.data;
-      const $ = cheerio.load(body);
-      const htmlTitle = $("title").text();
-      const step5 = ora(`${htmlTitle} 문제 정보 다운로드`).start();
-      step5.succeed();
-      const problemTitle = htmlTitle.split(": ")[1];
-      let sampleInput = 1,
-        sampleOutput = 1;
-      while ($(`#sample-input-${sampleInput}`).length > 0) {
-        let text = $(`#sample-input-${sampleInput}`).text();
-        if (text === "") {
-          ++sampleInput;
-          continue;
-        }
-        if (!text.endsWith("\n")) text += "\n";
-        fs.writeFileSync(
-          path.join(projectPath, `${newDirNum}`, `test/${sampleInput}.input`),
-          text
-        );
-        const step6 = ora(`파일 ${newDirNum}/test/${sampleInput} 생성`).start();
-        step6.succeed();
-        ++sampleInput;
-      }
-      while ($(`#sample-output-${sampleOutput}`).length > 0) {
-        let text = $(`#sample-output-${sampleOutput}`).text();
-        if (!text.endsWith("\n")) text += "\n";
-        fs.writeFileSync(
-          path.join(projectPath, `${newDirNum}`, `test/${sampleOutput}.answer`),
-          text
-        );
-        const step7 = ora(
-          `파일 ${newDirNum}/test/${sampleOutput} 생성`
-        ).start();
-        step7.succeed();
-        ++sampleOutput;
-      }
-      const step8 = ora(`README.md 수정`).start();
-      const timezoneOffset = new Date().getTimezoneOffset() * 60000;
-      const today = new Date(Date.now() - timezoneOffset)
-        .toISOString()
-        .substring(0, 10);
-      let readme = fs
-        .readFileSync(path.join(projectPath, "README.md"))
-        .toString();
-      readme = readme.replace(
-        "\n\n## 참고 링크",
-        `\n- ${today} ${problemTitle} [\\[문제\\]](https://www.acmicpc.net/problem/${problemNum}) [\\[코드\\]](https://github.com/niceb5y/algorithm-study/blob/niceb5y/${newDirNum}/main.cpp)\n\n## 참고 링크`
-      );
-      fs.writeFileSync(path.join(projectPath, "README.md"), readme);
-      step8.succeed();
-      const step9 = ora(`.travis.yml 수정`).start();
-      let travisConf = fs
-        .readFileSync(path.join(projectPath, ".travis.yml"))
-        .toString();
-      travisConf = travisConf.replace(
-        `"cd ${newDirNum - 1}"`,
-        `"cd ${newDirNum}"`
-      );
-      fs.writeFileSync(path.join(projectPath, ".travis.yml"), travisConf);
-      step9.succeed();
-      console.log();
-      console.log(chalk.green(`✨ 작업 완료 ✨`));
-      console.log();
-      console.log(
-        chalk.blue(
-          `'cd ${newDirNum}'(으)로 ${problemTitle}의 풀이를 시작하세요.`
-        )
-      );
-      console.log();
-    });
-});
+
+  const step5 = ora(`문제 정보 다운로드`).start();
+  const res = await axios.get(`https://www.acmicpc.net/problem/${problemNum}`);
+  const body = res.data;
+  const $ = cheerio.load(body);
+  const htmlTitle = $("title").text();
+  step5.succeed(`${htmlTitle} 문제 정보 다운로드`);
+
+  const step6 = ora(`테스트 케이스 입력 생성`).start();
+  let sampleInput = 1,
+    sampleOutput = 1;
+  while ($(`#sample-input-${sampleInput}`).length > 0) {
+    let text = $(`#sample-input-${sampleInput}`).text();
+    if (text === "") {
+      ++sampleInput;
+      continue;
+    }
+    if (!text.endsWith("\n")) text += "\n";
+    fs.writeFileSync(
+      path.join(projectPath, `${newDirNum}`, `test/${sampleInput}.input`),
+      text
+    );
+    ++sampleInput;
+  }
+  step6.succeed();
+
+  const step7 = ora(`테스트 케이스 출력 생성`).start();
+  while ($(`#sample-output-${sampleOutput}`).length > 0) {
+    let text = $(`#sample-output-${sampleOutput}`).text();
+    if (!text.endsWith("\n")) text += "\n";
+    fs.writeFileSync(
+      path.join(projectPath, `${newDirNum}`, `test/${sampleOutput}.answer`),
+      text
+    );
+    ++sampleOutput;
+  }
+  step7.succeed();
+
+  const step8 = ora(`README.md 수정`).start();
+  const timezoneOffset = new Date().getTimezoneOffset() * 60000;
+  const today = new Date(Date.now() - timezoneOffset)
+    .toISOString()
+    .substring(0, 10);
+  let readme = fs.readFileSync(path.join(projectPath, "README.md")).toString();
+  const problemTitle = htmlTitle.split(": ")[1];
+  readme = readme.replace(
+    "\n\n## 참고 링크",
+    `\n- ${today} ${problemTitle} [\\[문제\\]](https://www.acmicpc.net/problem/${problemNum}) [\\[코드\\]](https://github.com/niceb5y/algorithm-study/blob/niceb5y/${newDirNum}/main.cpp)\n\n## 참고 링크`
+  );
+  fs.writeFileSync(path.join(projectPath, "README.md"), readme);
+  step8.succeed();
+
+  const step9 = ora(`.travis.yml 수정`).start();
+  let travisConf = fs
+    .readFileSync(path.join(projectPath, ".travis.yml"))
+    .toString();
+  travisConf = travisConf.replace(`"cd ${newDirNum - 1}"`, `"cd ${newDirNum}"`);
+  fs.writeFileSync(path.join(projectPath, ".travis.yml"), travisConf);
+  step9.succeed();
+  console.log();
+  console.log(chalk.green(`✨ 작업 완료 ✨`));
+  console.log();
+  console.log(
+    chalk.blue(`'cd ${newDirNum}'(으)로 ${problemTitle}의 풀이를 시작하세요.`)
+  );
+  console.log();
+}
+
+main();

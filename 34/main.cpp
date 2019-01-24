718efbaaacece5f26ba228a3773eee927f8d5505
Seungho Kim <niceb5y@gmail.com>
2019-01-24 18:11:31 +0900

    Add “소수”

---
+++ 34/main.cpp
@@ -0,0 +1,60 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+// get_prime<N>(): [2...N]까지의 소수를 벡터로 반환하는 함수
+template <int N> vector<int> get_prime() {
+  // prime[]: 3부터 N까지의 홀수의 소수 여부를 저장
+  bool prime[N / 2 - 1];
+  // map(n): 3 이상의 홀수의 인덱스를 반환하는 람다식
+  // 3, 5, 7, ... -> 0, 1, 2, ...
+  auto map = [](int n) { return (n - 3) / 2; };
+  // ret: 함수의 반환값. 2를 초기 원소로 갖는 벡터
+  vector<int> ret({2});
+  // prime[] 초기화
+  for (auto &i : prime) {
+    i = true;
+  }
+  // 에라토스테네스의 체 알고리즘을 이용하여 소수를 판별한다
+  for (int i = 3; i < N; i += 2) {
+    // 해당 수가 소수인지 확인
+    if (prime[map(i)]) {
+      // 벡터에 집어넣기
+      ret.push_back(i);
+      // 해당 수의 배수는 소수가 아님, 짝수배는 검사를 건너뛴다.
+      for (int j = 3 * i; j < N; j += 2 * i) {
+        prime[map(j)] = false;
+      }
+    }
+  }
+  return ret;
+}
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(NULL);
+  // primes: 10000 이하의 소수들의 집합
+  vector<int> primes = get_prime<10000>();
+  // m, n: 소수의 범위 [m, n]
+  int m, n;
+  cin >> m >> n;
+  // lo: m 이상의 가장 작은 소수의 위치
+  auto lo = lower_bound(primes.begin(), primes.end(), m);
+  // hi: n 초과의 가장 작은 소수의 위치
+  auto hi = upper_bound(primes.begin(), primes.end(), n);
+  // 해당 범위 내에 소수가 존재하지 않으면 -1을 출력한다.
+  if (*lo > n) {
+    cout << -1 << "\n";
+    return 0;
+  }
+  // min: 해당 범위 내의 소수의 최솟값
+  // sum: 해당 범위 내의 소수의 합계
+  int min = *lo, sum = 0;
+  for (auto it = lo; it < hi; ++it) {
+    sum += *it;
+  }
+  // 결과를 출력한다.
+  cout << sum << "\n" << min << "\n";
+}
--- README.md
+++ README.md
@@ -39,6 +39,7 @@
 - 2019-01-23 LCS [\[문제\]](https://www.acmicpc.net/problem/9251) [\[코드\]](https://github.com/niceb5y/algorithm-study/blob/niceb5y/31/main.cpp)
 - 2019-01-24 골드바흐의 추측 [\[문제\]](https://www.acmicpc.net/problem/9020) [\[코드\]](https://github.com/niceb5y/algorithm-study/blob/niceb5y/32/main.cpp)
 - 2019-01-24 소수 찾기 [\[문제\]](https://www.acmicpc.net/problem/1978) [\[코드\]](https://github.com/niceb5y/algorithm-study/blob/niceb5y/33/main.cpp)
+- 2019-01-24 소수 [\[문제\]](https://www.acmicpc.net/problem/2581) [\[코드\]](https://github.com/niceb5y/algorithm-study/blob/niceb5y/34/main.cpp)
 
 ## 참고 링크
 

5ce4642c0055c3b8493bac60643a16e8e73ce0d7
미엘9 <ynifamily3@gmail.com>
2019-02-08 22:12:57 +0900

    피보나치 수 2
    Signed-off-by: 미엘9 <ynifamily3@gmail.com>

---
+++ bj-fibonacci-number2-2748/bj-fibonacci-number2-2748.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+unsigned long long f[91];
+
+int main(void)
+{
+    int n;
+    f[0] = 0;
+    f[1] = 1;
+    cin >> n;
+    // cin.get(); // 정수형일 경우 필요없음
+    for (int i = 2; i <= n; i++) {
+        f[i] = f[i - 1] + f[i - 2];
+    }
+    cout << f[n] << endl;
+    return 0;
+}

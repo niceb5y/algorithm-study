7271c2555a364dec13d2e48c4d768793f43acbaf
JSWww <tjddnjs9497@naver.com>
2019-02-11 21:05:14 +0900

    scv 자원 채취

--- dovelet/21stair/scv.py
+++ dovelet/21stair/scv.py
@@ -7,7 +7,7 @@ arr = [[int(i) for i in input().split()] for _ in range(n)]
 for i in range(n):
     for j in range(n):
         if i == j == 0: continue
-        elif i == 0: arr[i][j] = arr[i][j] + arr[i][j - 1]
-        elif j == 0: arr[i][j] = arr[i][j] + arr[i - 1][j]
-        else: arr[i][j] = max(arr[i - 1][j], arr[i][j - 1]) + arr[i][j]
+        elif i == 0: arr[i][j] += arr[i][j - 1]
+        elif j == 0: arr[i][j] += arr[i - 1][j]
+        else: arr[i][j] += max(arr[i - 1][j], arr[i][j - 1])
 print(arr[n - 1][n - 1])

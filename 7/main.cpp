a5f3fb272bb0c5ea02983513bfe01c0a74f5a5da
Seungho Kim <niceb5y@gmail.com>
2019-03-06 23:15:33 +0900

    Update “쿼드트리”

--- 7/main.cpp
+++ 7/main.cpp
@@ -1,4 +1,3 @@
-#include <cstdio>
 #include <iostream>
 
 using namespace std;
@@ -13,7 +12,6 @@ string quadtree(int x, int y, int size) {
   // 기저: 사이즈가 1이면 수를 리턴한다.
   if (size == 1)
     return (arr[y][x] == 1) ? "1" : "0";
-
   // 배열을 순회하면서 주어진 구역의 수가 모두 동일한지 확인한다.
   for (int i = y; i < y + size; ++i) {
     for (int j = x; j < x + size; ++j) {
@@ -35,12 +33,10 @@ int main() {
   // size: 입력의 사이즈
   int size;
   cin >> size;
-
   // buf: 한줄씩 값을 읽어올 변수
   string buf;
   // cin 버퍼 비우기
-  cin.get();
-
+  cin.ignore(1);
   for (int i = 0; i < size; ++i) {
     // 입력을 한줄씩 읽는다.
     getline(cin, buf);
@@ -49,7 +45,6 @@ int main() {
       arr[i][j] = (buf[j] == '1');
     }
   }
-
   // 결과물 출력
   cout << quadtree(0, 0, size) << endl;
 }

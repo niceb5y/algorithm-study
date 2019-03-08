b9b92c6252e23044ed4867a91516698287be5a29
Jonekeun Choi <ynifamily3@gmail.com>
2019-03-08 17:48:49 +0900

    숫자 구슬 <br> 그룹당 최댓값을 이분 탐색하면서 구슬 합 최댓값이 최소가 되게 만들어야 한다. <br> 특히 마지막에 출력할 때는 정답이 여러개가 될수도 있다. (어떻게 쪼개는지에 따라)

--- bj-number-bead-2613/bj-number-bead-2613.cpp
+++ bj-number-bead-2613/bj-number-bead-2613.cpp
@@ -5,6 +5,8 @@ using namespace std;
 int N, M; // 구슬 수, 나눌 그룹 수
 int beads[300]; // 구슬들에 적힌 숫자 집합
 int min_in_max;
+int a_table[301]; // 그룹별구슬개수 정답
+int aN = 1; //  그룹별구슬개수 수
 
 bool is_shrink_mid(int mid)
 {
@@ -14,6 +16,9 @@ bool is_shrink_mid(int mid)
     int nmb_group = 1; // 그룹 개수를 추정
     // 누적합이 mid를 초과한다면 그룹 수를 늘려야 한다.
     for (int i = 0; i < N - 1; i++) {
+        if (mid < beads[i]) {
+            return false; // 하나가 전체 미드레인지 초과시
+        }
         sum += beads[i];
         if ( sum > mid ) {
             nmb_group++;
@@ -58,22 +63,30 @@ int main(void)
 
     cout << min_in_max << endl;
 
-    // 사혼의 구슬조각들
-    // 그냥 각자 최댓값만 안 넘게 해서 출력함.
-    // 어차피 저지 되겠지 뭐 정답이 여러개일수도 있음
     int sum = 0, incount = 0;
     for (int i = 0; i < N; i++) {
         sum += beads[i];
+        incount++;
         if (sum > min_in_max) {
             sum = beads[i];
-            cout << incount << ' ';
-            incount = 0;
+            a_table[aN++] = incount - 1;
+            incount = 1; // curr
         }
-        incount++;
     }
-    // 마지막 구슬
-    cout << incount << endl;
-    
+    // 마지막 구슬 그룹
+    a_table[aN] = incount;
+    int diff = (M - aN); // 잉여 그룹으로 분배하기 위함
+    for (int i = 1; i <= aN; i++) {
+        if (a_table[i] != 1) {
+            // 잉여 그룹이 있을경우 적절히 구슬들을 임의의 그룹으로 쪼개 주는데
+            while (a_table[i] >= 2 && diff-- >= 1) {
+                cout << "1 ";
+                a_table[i]--;
+            }
+        }
+        cout << a_table[i] << ' ';
+    }
+    cout << endl;
 
     return 0;
 }
